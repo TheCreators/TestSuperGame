1b02d04f5cf2d012dbf4f29ad2c72c4cc02da539
Timofey Kochetov <herew26@gmail.com>
2022-08-26 02:46:18 +0300

    Update DFsdf.cs

--- Assets/DFsdf.cs
+++ Assets/DFsdf.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class DFsdf : MonoBehaviour
 {
-    int my_property;
+    int my_property
+    ;
     
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+     }
 
     // Update is called once per frame
-    void Update()
+ void Update()
     {
         
     }
